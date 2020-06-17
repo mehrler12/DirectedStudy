dee12b57a01e31fae20c04f2806841506d60f27d
Matthew Ehrler <627935@gmail.com>
2020-06-17 16:28:52 -0700

    Make state matrix 2d

---
+++ CUDAFilter/kalman.cu
@@ -0,0 +1,8 @@
+#include <chrono>
+#include <iostream>
+#include "C:\Users\62793\CSC591\DirectedStudy\data\measurements.hpp"
+
+int main(){
+    auto start_time = std::chrono::system_clock::now();
+    std::cout << measurements[0][:][:];
+}
